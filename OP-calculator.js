fc8048ccf4f721d1b8e96fe7b64219516ef35b3b
StefanTampu <114590807+StefanTampu@users.noreply.github.com>
2023-04-19 10:24:56 -0400

    Create operate() to compute various operations

--- OP-calculator.js
+++ OP-calculator.js
@@ -1,3 +1,7 @@
+//Operation variables
+let a;
+let b;
+let operation;
 
 //Operational functions
 
@@ -8,3 +12,17 @@ const subtract = arr => arr.reduce((a,b) => a-b);
 const multiply = arr => arr.reduce((a,b) => a*b, 1);
 
 const divide = arr => arr.reduce((a,b) => a/b);
+
+const operate = (a, b, operation) => {
+    let result;    
+    if (operation === "add"){
+        result = add([a, b]);
+    } else if (operation === "subtract"){
+        result = subtract([a, b]);
+    } else if (operation === "multiply"){
+        result = multiply([a, b]);
+    } else if (operation === "divide"){
+        result = divide([a, b])
+    }
+    console.log(result);
+}

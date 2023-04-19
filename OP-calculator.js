9cc5d143bfd0af54fcf3332b658db8adb6368649
StefanTampu <114590807+StefanTampu@users.noreply.github.com>
2023-04-19 07:11:32 -0400

    Create functions for basic operations

---
+++ OP-calculator.js
@@ -0,0 +1,10 @@
+
+//Operational functions
+
+const add = arr => arr.reduce((a,b) => a+b, 0);
+
+const subtract = arr => arr.reduce((a,b) => a-b);
+
+const multiply = arr => arr.reduce((a,b) => a*b, 1);
+
+const divide = arr => arr.reduce((a,b) => a/b);

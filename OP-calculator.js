ab5c3977845de65ea86c451f10e2085a3722b7c4
StefanTampu <114590807+StefanTampu@users.noreply.github.com>
2023-04-20 08:00:54 -0400

    Create createNumber() to concatinate two num strings as digits

--- OP-calculator.js
+++ OP-calculator.js
@@ -5,16 +5,23 @@ let operator = null;
 
 const computation = document.getElementById("computation");
 const answer = document.getElementById("answer");
-const operatorBtn = document.querySelectorAll(".operator-btn");
-const numberBtn = document.querySelectorAll("number-btn");
+const operatorBtns = document.querySelectorAll(".operator-btn");
+const numberBtns = document.querySelectorAll("number-btn");
 const plusMinus = document.getElementById("&plusmn;");
 const dot = document.getElementById("dot");
 const backSpace = document.getElementById("back-spc");
 const cancel = document.getElementById("cancel");
 const equals = document.getElementById("equals");
-
-
 const buttons = document.querySelectorAll("button");
+
+const createNumber = (a,b) => {
+    a.concat(b);
+}
+
+numberBtns.forEach((button) => {
+    button.addEventListener("click", () => {})
+})
+
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         computation.textContent = `${button.id}`;
